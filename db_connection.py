ea6271b831e78715b9f98c8fd736803a32e7dfff
shivabollam <shivabollam07@gmail.com>
2024-02-13 21:30:52 +0530

    Updated requirements

--- db_connection.py
+++ db_connection.py
@@ -4,7 +4,7 @@ def get_connection():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password='',
+        password='shiva@437',
         database='walmart'
     )
 
